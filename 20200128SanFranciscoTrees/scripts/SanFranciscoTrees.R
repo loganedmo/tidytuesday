edd66fe503a5bb804009f2ae00efb4119efbfeb6
Logan Edmonds <loganpe@okstate.edu>
2020-01-28 13:21:30 -0600

    add library calls

---
+++ 20200128SanFranciscoTrees/scripts/SanFranciscoTrees.R
@@ -0,0 +1,11 @@
+#####################################################################
+# TIDY TUESDAY JAN. 28TH 2020: SAN FRANCISCO TREES
+#
+#
+#
+# https://github.com/rfordatascience/tidytuesday/blob/master/data/2020/2020-01-28/readme.md
+#####################################################################
+
+library(dplyr)
+library(ggplot2)
+library(readr)
