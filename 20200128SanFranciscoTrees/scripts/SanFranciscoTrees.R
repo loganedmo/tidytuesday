b839c08fd0a8b522da432e05dff56b5168d38d09
Logan Edmonds <loganedmonds94@gmail.com>
2020-01-28 21:03:15 -0600

    add ggmap plot

--- 20200128SanFranciscoTrees/scripts/SanFranciscoTrees.R
+++ 20200128SanFranciscoTrees/scripts/SanFranciscoTrees.R
@@ -26,19 +26,34 @@ sf_trees <-
 #############################################
 # plantings by year
 # NA dates are pre 1955 trees
-# consider removing pre-1972: sparse data
+# ~ 2,000 rows with missing lat/lon
 #############################################
 
+sf_trees <- 
 sf_trees %>% 
+  filter(date >= '1972-01-01' & # sparse data prior to 1972
+         !is.na(latitude) & !is.na(longitude)) %>% 
   mutate(date = if_else(is.na(date), 
                 as_date('1955-01-01'), date))
 
 #############################################
 # static mapping of SF
 # use Google's Static Map API service
+# SF lat/lon - 37.7749° N, 122.4194° W
 #############################################
 
 ggmap::register_google(key = "AIzaSyA3dDU9SYrydFwnDA8wKiLwSqP0AnivDG0")
 
-ggmap(get_googlemap
+# raster object
+sf_map <- 
+get_googlemap(center = c(lon = -122.4194, 37.7749),
+              zoom = 12, scale = 2, maptype = "terrain")
+
+ggmap(sf_map) +
+  geom_point(data = sf_trees, 
+             aes(x = longitude, y = latitude, color = date),
+             size = 0.1, alpha = 0.5) +
+  theme(axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank())
 
