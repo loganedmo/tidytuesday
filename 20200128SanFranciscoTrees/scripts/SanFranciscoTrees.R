4fbcd0ee0d340c5315dc2809635402196fb8a3f1
Logan Edmonds <loganedmonds94@gmail.com>
2020-01-28 22:53:32 -0600

    add lat/lon boundary box denisty map

--- 20200128SanFranciscoTrees/scripts/SanFranciscoTrees.R
+++ 20200128SanFranciscoTrees/scripts/SanFranciscoTrees.R
@@ -15,6 +15,7 @@ library(ggmap)
 library(ggplot2)
 library(lubridate)
 library(readr)
+library(RColorBrewer)
 
 #############################################
 # import data
@@ -44,16 +45,47 @@ sf_trees %>%
 
 ggmap::register_google(key = "AIzaSyA3dDU9SYrydFwnDA8wKiLwSqP0AnivDG0")
 
-# raster object
-sf_map <- 
-get_googlemap(center = c(lon = -122.4194, 37.7749),
-              zoom = 12, scale = 2, maptype = "terrain")
+# lat/lon boundary for tree locations
+sf_tree_bb <- c(left = -122.53,
+                right = -122.35,
+                bottom = 37.70,
+                top = 37.82)
 
-ggmap(sf_map) +
-  geom_point(data = sf_trees, 
-             aes(x = longitude, y = latitude, color = date),
-             size = 0.1, alpha = 0.5) +
+# Stamen Map raster object
+sf_map_stamen <- 
+  get_stamenmap(bbox = sf_tree_bb,
+                zoom = 13,
+                maptype = "terrain",
+                color = "bw",
+                force = TRUE)
+
+ggmap(sf_map_stamen, extent = "device") +
+  stat_density_2d(data = sf_trees, 
+                  aes(x = longitude, y = latitude,
+                      fill = ..level..),
+                  geom = "polygon",
+                  alpha = 0.1) +
+  scale_fill_gradientn(colors = brewer.pal(7, "BuGn")) +
   theme(axis.text = element_blank(),
         axis.ticks = element_blank(),
-        axis.title = element_blank())
+        axis.title = element_blank(),
+        plot.background = element_rect(fill = "#f5f5f2", 
+                                       color = NA),
+        panel.background = element_rect(fill = "#f5f5f2",                                             color = NA), 
+        legend.background = element_rect(fill = "#f5f5f2",                                             color = NA),
+        panel.border = element_blank())
 
+ggmap(sf_map_stamen) +
+  stat_summary2d()
+
+#############################################
+# deprecated code
+# Google Maps API offers less flexibility
+# than Stamen Maps
+#############################################
+
+# Google Maps raster object
+sf_map_google <- 
+  get_googlemap(center = c(lon = -122.434, lat = 37.761),
+                zoom = 12, scale = 2, 
+                maptype = "terrain")
