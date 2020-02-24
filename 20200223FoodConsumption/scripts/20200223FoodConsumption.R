ee02cfbc8cd11e818021e11eb70b9b5f6136d57a
Logan Edmonds <loganedmonds94@gmail.com>
2020-02-23 21:43:25 -0600

    data cleaning for waffle plot

--- 20200223FoodConsumption/scripts/20200223FoodConsumption.R
+++ 20200223FoodConsumption/scripts/20200223FoodConsumption.R
@@ -12,6 +12,7 @@ library(dplyr)
 library(forcats)
 library(ggplot2)
 library(readr)
+library(waffle)
 
 #############################################
 # read data
@@ -20,11 +21,30 @@ library(readr)
 
 food_consumption <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-18/food_consumption.csv')
 
+food_consumption <- 
 food_consumption %>% 
   mutate(continent = countrycode(country, "country.name", "continent"),
-         region = countrycode(country, "country.name", "region"),
-         country = as_factor(country),
-         food_category = as_factor(food_category),
-         continent = as_factor(continent),
-         region = as_factor(region))
+         region = countrycode(country, "country.name", "region")) %>% 
+  mutate_if(is.character, as_factor)
 
+#############################################
+# waffle chart
+#
+# example blog:
+# https://www.nsgrantham.com/food-carbon-footprint
+#############################################
+
+# example waffle plot code
+ggplot() +
+  geom_waffle(data = carbon_footprints, 
+              aes(values = co2 / 10, fill = food),
+              color = "#F8F8FF", size = .25, n_rows = 10, flip = TRUE)
+
+meat_sources <- c("Pork", "Poultry", "Beef", "Lamb & Goat", "Fish")
+
+meat_consumption <- 
+food_consumption %>% 
+  filter(food_category %in% meat_sources) %>% 
+  group_by(continent, food_category) %>% 
+  summarise(consumption = sum(consumption)) %>% 
+  mutate(consumption_perc = consumption/sum(consumption))
