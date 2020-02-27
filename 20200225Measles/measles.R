e830ee92d4ab3b21788307fcdcf2da95ad879138
Logan Edmonds <loganedmonds94@gmail.com>
2020-02-26 23:03:19 -0600

    correct county names

--- 20200225Measles/measles.R
+++ 20200225Measles/measles.R
@@ -39,8 +39,25 @@ ok_counties <-
                plot = FALSE, 
                fill = TRUE)) %>% 
   filter(str_detect(ID, "oklahoma")) %>% 
+  # convert to uppercase for join
   mutate(state = firstup(str_extract(ID, "oklahoma")),
          county = firstup(str_extract(ID, "[^,]*$")))
+
+# firstup() function did not correctly parse
+# counites with multiple uppercase chars (e.g. "McClain")
+# or counties with spaces ("Le Flore")
+correct_names <- 
+  c("Le Flore", "McClain", "McCurtain",
+    "McIntosh", "Roger Mills")
+
+# find mismatch indices
+county_miscodes <- 
+  which(!ok_counties$county %in% ok_measles$county)
+
+# replace mismatches with correct names
+ok_counties$county[county_miscodes] <- correct_names
+
+
 
 ok_all <- 
   ok_counties %>% 
